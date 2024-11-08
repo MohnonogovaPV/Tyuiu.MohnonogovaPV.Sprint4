59f737bc1b8c085abdbd2ab6abf4d4db2bae7b63
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-11-08 18:19:39 +0500

    Task1

---
+++ Tyuiu.MohnonogovaPV.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint4.Task1.V3.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint4.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                                *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                                             *");
+            Console.WriteLine("* Задание #1                                                                               *");
+            Console.WriteLine("* Вариант #3                                                                               *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                 *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный значениями               *");
+            Console.WriteLine("* с клавиатуры в диапазоне от 1 до 9 подсчитать произведение четных элементов массива.     *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элемента массива: ");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
